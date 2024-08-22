064ec1f2b9da3d1b60b2692ea8afa68f5b123f35
zarganum <petrobizam@gmail.com>
2024-08-22 19:21:46 +0000

    test expired creds

--- tests/test_changepw.py
+++ tests/test_changepw.py
@@ -9,14 +9,18 @@ import krb5
 @pytest.mark.requires_api("set_password")
 def test_set_password(realm: k5test.K5Realm) -> None:
     ctx = krb5.init_context()
-    princ = krb5.parse_name_flags(ctx, realm.user_princ.encode())
+    princ = krb5.parse_name_flags(ctx, f"userexp@{realm.realm}".encode())
     opt = krb5.get_init_creds_opt_alloc(ctx)
-    creds = krb5.get_init_creds_password(ctx, princ, opt, realm.password("user").encode())
+
+    with pytest.raises(krb5.Krb5Error) as exc:
+        krb5.get_init_creds_password(ctx, princ, opt, password=realm.password("user").encode())
+    assert exc.value.err_code == -1765328361
+
+    creds = krb5.get_init_creds_password(ctx, princ, opt, realm.password("user").encode(), in_tkt_service=b"kadmin/changepw")
     assert isinstance(creds, krb5.Creds)
 
     newpw = realm.password("user").encode()
     result = krb5.set_password(ctx, creds, newpw, princ)
-    raise ValueError(result)
 
     creds = krb5.get_init_creds_password(ctx, princ, opt, newpw)
     assert isinstance(creds, krb5.Creds)
