deb5354654d4659d292ee8e5f0244449c86cdad3
zarganum <petrobizam@gmail.com>
2024-08-23 11:46:40 +0000

    removed heimdal testing code

--- tests/test_changepw.py
+++ tests/test_changepw.py
@@ -8,37 +8,12 @@ import krb5
 
 @pytest.mark.requires_api("set_password")
 def test_set_password(realm: k5test.K5Realm) -> None:
-    if realm.provider == "mit":
-        realm.start_kadmind()
-    elif realm.provider == "heimdal" and os.path.isfile("/etc/redhat-release"):
-        # This is a RHEL start/stop demonstration for Heimdal
-        realm.kadmind = "/usr/libexec/heimdal-kadmind"
-        kadmind_args = [
-            realm.kadmind,
-            "--config-file=%s" % (realm.env["KRB5_CONFIG"]),
-            # "--keytab=%s" % (realm.keytab),
-            "--ports=%s" % (realm.portbase + 1),
-        ]
 
-        realm._kadmind_proc = realm._start_daemon(kadmind_args)
+    if realm.provider != "mit":
+        # Heimdal testing requires complicated kadmind and kpasswdd setup
+        return
 
-        changepw_keytab = os.path.join(realm.tmpdir, "changepw.keytab")
-        realm.run_kadminl(
-            [
-                "ext_keytab",
-                "-k",
-                changepw_keytab,
-                "kadmin/changepw",
-            ]
-        )
-
-        kpasswdd_args = [
-            "/usr/libexec/kpasswdd",
-            "--config-file=%s" % (realm.env["KRB5_CONFIG"]),
-            "--keytab=%s" % (changepw_keytab),
-            "--port=%s" % (realm.portbase + 2),
-        ]
-        kpasswdd_proc = realm._start_daemon(kpasswdd_args, realm.env)
+    realm.start_kadmind()
 
     princ_name = "exp@" + realm.realm
     old_password = realm.password("userexp")
@@ -46,25 +21,8 @@ def test_set_password(realm: k5test.K5Realm) -> None:
     empty_password = ""
     new_password = realm.password("user")
 
-    if realm.provider == "mit":
-        realm.run_kadminl(["addpol", "-minlength", "6", "-minclasses", "2", "pwpol"])
-        realm.run_kadminl(["addprinc", "-pw", old_password, "-policy", "pwpol", "+needchange", princ_name])
-    else:
-        # This demonstrates how to create user with expired password on Heimdal
-        realm.run_kadminl(
-            [
-                "add",
-                "-p",
-                old_password,
-                "--max-ticket-life=1 day",
-                "--max-renewable-life=1 week",
-                "--expiration-time=never",
-                "--pw-expiration-time=never",
-                "--policy=default",
-                "--attributes=requires-pw-change",
-                princ_name,
-            ]
-        )
+    realm.run_kadminl(["addpol", "-minlength", "6", "-minclasses", "2", "pwpol"])
+    realm.run_kadminl(["addprinc", "-pw", old_password, "-policy", "pwpol", "+needchange", princ_name])
 
     ctx = krb5.init_context()
     princ = krb5.parse_name_flags(ctx, princ_name.encode())
@@ -81,16 +39,14 @@ def test_set_password(realm: k5test.K5Realm) -> None:
     assert result_code == 0
 
     assert result_code != 0
-    if realm.provider == "mit":
-        assert result_code_string.find(b"rejected") > 0
-        assert result_string.find(b"too short") > 0
+    assert result_code_string.find(b"rejected") > 0
+    assert result_string.find(b"too short") > 0
 
     (result_code, result_code_string, result_string) = krb5.change_password(ctx, creds, weak_password.encode())
 
     assert result_code != 0
-    if realm.provider == "mit":
-        assert result_code_string.find(b"rejected") > 0
-        assert result_string.find(b"too short") > 0
+    assert result_code_string.find(b"rejected") > 0
+    assert result_string.find(b"too short") > 0
 
     (result_code, result_code_string, result_string) = krb5.change_password(ctx, creds, new_password.encode())
     assert result_code == 0
@@ -98,8 +54,7 @@ def test_set_password(realm: k5test.K5Realm) -> None:
     creds = krb5.get_init_creds_password(ctx, princ, opt, new_password.encode())
     assert isinstance(creds, krb5.Creds)
 
-    if realm.provider == "mit":
-        realm.run_kadminl(["modprinc", "-pw", old_password, "-policy", "pwpol", "+needchange", princ_name])
+    realm.run_kadminl(["modprinc", "-pw", old_password, "-policy", "pwpol", "+needchange", princ_name])
 
     with pytest.raises(krb5.Krb5Error) as exc:
         krb5.get_init_creds_password(ctx, princ, opt, password=old_password.encode())
@@ -136,8 +91,5 @@ def test_set_password(realm: k5test.K5Realm) -> None:
 
     realm.run_kadminl(["delprinc", "-force", princ_name])
     realm.run_kadminl(["delpol", "-force", "pwpol"])
-
-    if kpasswdd_proc:
-        realm._stop_daemon(kpasswdd_proc)
 
     realm.stop_kadmind()
